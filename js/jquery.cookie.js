3f499716e80e9adaf8fded6ec4ad84264b6a2c31
klayman <filippov.sem@gmail.com>
2012-08-08 20:43:21 +0700

    Concept files for the new (2012) version

--- js/jquery.cookie.js
+++
@@ -1,6 +0,0 @@
-/* Cookie plugin
- Copyright (c) 2006 Klaus Hartl (stilbuero.de)
- Dual licensed under the MIT and GPL licenses:
- http://www.opensource.org/licenses/mit-license.php
- http://www.gnu.org/licenses/gpl.html */
-eval(function(p,a,c,k,e,r){e=function(c){return(c<a?'':e(parseInt(c/a)))+((c=c%a)>35?String.fromCharCode(c+29):c.toString(36))};if(!''.replace(/^/,String)){while(c--)r[e(c)]=k[c]||e(c);k=[function(e){return r[e]}];e=function(){return'\\w+'};c=1};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p}('n.5=v(a,b,c){4(7 b!=\'z\'){c=c||{};4(b===q){b=\'\';c.3=-1}2 d=\'\';4(c.3&&(7 c.3==\'p\'||c.3.r)){2 e;4(7 c.3==\'p\'){e=x D();e.H(e.B()+(c.3*F*t*t*w))}o{e=c.3}d=\'; 3=\'+e.r()}2 f=c.8?\'; 8=\'+(c.8):\'\';2 g=c.9?\'; 9=\'+(c.9):\'\';2 h=c.u?\'; u\':\'\';6.5=[a,\'=\',A(b),d,f,g,h].E(\'\')}o{2 j=q;4(6.5&&6.5!=\'\'){2 k=6.5.y(\';\');G(2 i=0;i<k.m;i++){2 l=n.C(k[i]);4(l.s(0,a.m+1)==(a+\'=\')){j=I(l.s(a.m+1));J}}}K j}};',47,47,'||var|expires|if|cookie|document|typeof|path|domain|||||||||||||length|jQuery|else|number|null|toUTCString|substring|60|secure|function|1000|new|split|undefined|encodeURIComponent|getTime|trim|Date|join|24|for|setTime|decodeURIComponent|break|return'.split('|'),0,{}))
